6a8c4b2f3b3d7f247833af897ac7bde5cbf948c0
alpcihan <alpcihan.ac@gmail.com>
2022-12-19 21:25:13 +0100

    add opencv v4.1.2

--- src/voxel-carving.cpp
+++ src/voxel-carving.cpp
@@ -1,4 +1,30 @@
-int main()
+// Uncomment the following line if you are compiling this code in Visual Studio
+// #include "stdafx.h"
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
 {
+    // create a pink image
+    cv::Mat image(320, 320, CV_8UC3, cv::Scalar(155, 0, 155));
+
+    // check for failure
+    if (image.empty())
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); 
+        return -1;
+    }
+
+    cv::String windowName = "pink color"; 
+    cv::namedWindow(windowName); 
+
+    cv::imshow(windowName, image); 
+    cv::waitKey(0); 
+    cv::destroyWindow(windowName); 
+
     return 0;
 }
