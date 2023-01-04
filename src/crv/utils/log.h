da371cca5ff4978df1f908f28b1bd3c13cf2e642
alpcihan <alpcihan.ac@gmail.com>
2023-01-04 21:29:41 +0100

    add log toggle

--- src/crv/utils/log.h
+++ src/crv/utils/log.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include <iostream>
-#include <cstdlib>
 
 #define CRV_ERROR(message)  std::cerr << "[ERROR]: " << message << "\n";
+
+#ifndef CRV_DISABLE_INFO_LOGS
+
 #define CRV_INFO(message)   std::cout << "[INFO]: " << message << "\n";
+
+#else
+
+#define CRV_INFO(message)
+
+#endif
